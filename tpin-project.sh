319da851cfe1fa65ecf507d5e0b6b3df681a4d15
Marco Ferrari <ferrari.marco@gmail.com>
2015-05-22 19:17:55 +0200

    Working on the implementation of rrdgraph REST endpoint.
    Added parameters

--- tpin-project.sh
+++ tpin-project.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-args=("$@")
+while test $# -gt 0
+do
+    case "$1" in
+        --setup)
+		  echo "Setting up environment..."
+		  apt-get install rrdtool librrds-perl librrd-dev
+		  pip install rrdtool
+		  cd /home/user/workspace
+		  git clone https://github.com/ferrarimarco/tpin-2015-project.git
+            ;;
+        --install) 
+		  cd /home/user/workspace/tpin-2015-project
+		  git pull
+		  cp *.py /home/user/workspace/ryu/ryu/app/
+		  cd /home/user/workspace/ryu
+		  python ./setup.py install
+            ;;
+        --run-mininet)
+		 cd /home/user/workspace/Dreamer-Mininet-Extensions
+		 ./mininet_deployer.py --topology topo/topo_vll_pw.json
+            ;;
+        --run-ryu)
+		  cd /home/user/workspace/tpin-2015-project
+		  ryu-manager --observe-links traffic_monitor.py ofctl_rest.py rrdmanager.py
+            ;;
 
-if [args[0] == "setup"]; then
-  echo "Setting up environment..."
-  apt-get install rrdtool librrds-perl librrd-dev
-  pip install rrdtool
-  cd /home/user/workspace
-  git clone https://github.com/ferrarimarco/tpin-2015-project.git
-elif [args[0] == "install"]; then
-  cd /home/user/workspace/tpin-2015-project
-  git pull
-  cp *.py /home/user/workspace/ryu/ryu/app/
-  cd /home/user/workspace/ryu
-  python ./setup.py install
-elif [args[0] == "run-mininet"]; then
-  cd /home/user/workspace/Dreamer-Mininet-Extensions
-  ./mininet_deployer.py --topology topo/topo_vll_pw.json
-elif [args[0] == "run-ryu"]; then
-  cd /home/user/workspace/tpin-2015-project
-  ryu-manager --observe-links traffic_monitor.py ofctl_rest.py rrdmanager.py
-else
-  echo "Mode not supported."
+        --*) echo "bad option $1"
+            ;;
+        *) echo "argument $1"
+            ;;
+    esac
+    shift
+done
