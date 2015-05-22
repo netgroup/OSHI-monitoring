b11ef642a15b7caa1d856d39b892f07452ebccf1
Marco Ferrari <ferrari.marco@gmail.com>
2015-05-22 18:32:19 +0200

    Early support for maintenance script.

--- tpin-project.sh
+++ tpin-project.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+args=("$@")
+
+if [args[0] == "setup"]; then
+  echo "Setting up environment..."
+  apt-get install rrdtool librrds-perl librrd-dev
+  pip install rrdtool
+  cd /home/user/workspace
+  git clone https://github.com/ferrarimarco/tpin-2015-project.git
+elif [args[0] == "install"]; then
+  cd /home/user/workspace/tpin-2015-project
+  git pull
+  cp *.py /home/user/workspace/ryu/ryu/app/
+  cd /home/user/workspace/ryu
+  python ./setup.py install
+elif [args[0] == "run-mininet"]; then
+  cd /home/user/workspace/Dreamer-Mininet-Extensions
+  ./mininet_deployer.py --topology topo/topo_vll_pw.json
+elif [args[0] == "run-ryu"]; then
+  cd /home/user/workspace/tpin-2015-project
+  ryu-manager --observe-links traffic_monitor.py ofctl_rest.py rrdmanager.py
+else
+  echo "Mode not supported."
