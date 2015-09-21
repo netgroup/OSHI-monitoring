7062de7f63a068adbd1aa67a6ffeb34cb62d4ab2
Marco Ferrari <ferrari.marco@gmail.com>
2015-09-21 18:33:00 +0200

    Reworked constructDataSource.
     * Removed dev_id parameter because the dev_id is in the file name
     * Renamed constructDataSource to a more idiomatic _build_rrd_data_source_definition

--- rrdmanager.py
+++ rrdmanager.py
@@ -44,7 +44,7 @@ class RRDManager(object):
     def get_current_time():
         return int(math.floor(time.time()))
 
-    def constructDataSource(self, dev_id, port_n, eth_type):
+    def _build_rrd_data_source_definition(self, port_n, eth_type):
         return str(str(port_n) + '_' + str(eth_type))
 
     def __init__(self, filename, device, eth_types):
@@ -62,7 +62,7 @@ class RRDManager(object):
         for dev_id in sorted(self.device):
             for port_n in self.device[dev_id]:
                 for eth_type in self.eth_types:
-                    temp = str(self.constructDataSource(dev_id, port_n, eth_type))
+                    temp = str(self._build_rrd_data_source_definition(port_n, eth_type))
                     self.raw_data_sources.append(temp)
                     data_source = 'DS:' + temp + ':GAUGE:600:U:U'
                     log.debug("Build RRD data source from %s . Result: %s", temp, data_source)
@@ -106,5 +106,5 @@ device['PEO4'] = [2,3,5]
 
 rrdman = RRDManager('test.rrd', device, ["0800", "0806", "0000"])
 time.sleep(10)
-rrdman.update([rrdman.constructDataSource('PEO1', 1, "0800"), rrdman.constructDataSource('PEO2', 1, "0806"), rrdman.constructDataSource('PEO3', 1, "0000")],[13000,15005,13501])
+rrdman.update([rrdman.construct_rrd_data_source('PEO1', 1, "0800"), rrdman.construct_rrd_data_source('PEO2', 1, "0806"), rrdman.construct_rrd_data_source('PEO3', 1, "0000")],[13000,15005,13501])
 '''
