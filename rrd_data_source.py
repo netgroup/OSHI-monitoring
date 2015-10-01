85f446a2f39e37cffb8148e699da346d572ded8d
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-01 16:10:47 +0200

    Implemented constructor for RRDDataSource. For #2

--- rrd_data_source.py
+++ rrd_data_source.py
@@ -2,3 +2,8 @@ class RRDDataSource(object):
     name = ""
     data_source_type = ""
     heartbeat = ""
+
+    def __init__(self, name, data_source_type, heartbeat):
+        self.name = name
+        self.data_source_type = data_source_type
+        self.heartbeat = heartbeat
