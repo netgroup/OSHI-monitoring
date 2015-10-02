7186faebdb22f4aacffb2e0603fe6918f6b89ee7
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-02 16:27:35 +0200

    Add temp_value (optional) parameter to RRDDataSource constructor.

--- rrd_data_source.py
+++ rrd_data_source.py
@@ -4,7 +4,8 @@ class RRDDataSource(object):
     heartbeat = ""
     temp_value = 0
 
-    def __init__(self, name, data_source_type, heartbeat):
+    def __init__(self, name, data_source_type, heartbeat, temp_value=None):
         self.name = name
         self.data_source_type = data_source_type
         self.heartbeat = heartbeat
+        self.temp_value = temp_value
