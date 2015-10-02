674703e5185f15e4ee2c671d092c7278f4e93676
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-02 17:01:26 +0200

    Implemented string representation to RRDDataSource

--- rrd_data_source.py
+++ rrd_data_source.py
@@ -9,3 +9,7 @@ class RRDDataSource(object):
         self.data_source_type = data_source_type
         self.heartbeat = heartbeat
         self.temp_value = temp_value
+
+    def __str__(self):
+        return "RRD Data Source(name=" + self.name + ", data_source_type=" + self.data_source_type + ", heartbeat=" \
+               + self.heartbeat + ", temp_value=" + self.temp_value
