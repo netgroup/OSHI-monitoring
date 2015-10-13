f2ef1015635cdc9d01ea07b1ffec0323fb344730
Marco Ferrari <ferrari.marco@gmail.com>
2015-10-13 19:55:15 +0200

    Removed buffers in SwitchStats. For #3
    This leads to a simplification of stat computation.
    
    Also implemented a test for SwitchStats. For now there are tests for RX_BYTES stat only, but the plan is to implement tests for all stats in SwitchStats.

--- switch_stats.py
+++ switch_stats.py
@@ -2,23 +2,6 @@ import logging
 import os
 import config
 
-_RX_BYTES_BUFFER = 'rx_bytes_buffer'
-_RX_BYTES_BUFFER_INDEX = 'rx_bytes_buffer_index'
-_TX_BYTES_BUFFER = 'tx_bytes_buffer'
-_TX_BYTES_BUFFER_INDEX = 'tx_bytes_buffer_index'
-_RX_PACKETS_BUFFER = 'rx_packets_buffer'
-_RX_PACKETS_BUFFER_INDEX = 'rx_packets_buffer_index'
-_TX_PACKETS_BUFFER = 'tx_packets_buffer'
-_TX_PACKETS_BUFFER_INDEX = 'tx_packets_buffer_index'
-_SDN_RX_BYTES_BUFFER = 'sdn_rx_bytes_buffer'
-_SDN_RX_BYTES_BUFFER_INDEX = 'sdn_rx_bytes_buffer_index'
-_SDN_TX_BYTES_BUFFER = 'sdn_tx_bytes_buffer'
-_SDN_TX_BYTES_BUFFER_INDEX = 'sdn_tx_bytes_buffer_index'
-_SDN_RX_PACKETS_BUFFER = 'sdn_rx_packets_buffer'
-_SDN_RX_PACKETS_BUFFER_INDEX = 'sdn_rx_packets_buffer_index'
-_SDN_TX_PACKETS_BUFFER = 'sdn_tx_packets_buffer'
-_SDN_TX_PACKETS_BUFFER_INDEX = 'sdn_tx_packets_buffer_index'
-
 RX_BYTES = 'rx_bytes'
 TX_BYTES = 'tx_bytes'
 RX_PACKETS = 'rx_packets'
@@ -69,26 +52,13 @@ class SwitchStats:
         """
         self.ports[port_number] = {}
         self.ports[port_number][RX_BYTES] = 0
-        self.ports[port_number][_RX_BYTES_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_RX_BYTES_BUFFER_INDEX] = 0
         self.ports[port_number][TX_BYTES] = 0
-        self.ports[port_number][_TX_BYTES_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_TX_BYTES_BUFFER_INDEX] = 0
         self.ports[port_number][RX_PACKETS] = 0
-        self.ports[port_number][_RX_PACKETS_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_RX_PACKETS_BUFFER_INDEX] = 0
         self.ports[port_number][TX_PACKETS] = 0
-        self.ports[port_number][_TX_PACKETS_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_TX_PACKETS_BUFFER_INDEX] = 0
-        # SDN BUFFERS
-        self.ports[port_number][_SDN_RX_BYTES_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_SDN_RX_BYTES_BUFFER_INDEX] = 0
-        self.ports[port_number][_SDN_TX_BYTES_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_SDN_TX_BYTES_BUFFER_INDEX] = 0
-        self.ports[port_number][_SDN_RX_PACKETS_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_SDN_RX_PACKETS_BUFFER_INDEX] = 0
-        self.ports[port_number][_SDN_TX_PACKETS_BUFFER] = [0] * config.DELTA_WINDOW
-        self.ports[port_number][_SDN_TX_PACKETS_BUFFER_INDEX] = 0
+        self.ports[port_number][SDN_RX_BYTES] = 0
+        self.ports[port_number][SDN_TX_BYTES] = 0
+        self.ports[port_number][SDN_RX_PACKETS] = 0
+        self.ports[port_number][SDN_TX_PACKETS] = 0
 
     def set_port_name(self, port_number, port_name):
         """
@@ -128,10 +98,7 @@ class SwitchStats:
         :param port_number:
         :return: Partner port number or -1 if no relationship has been defined for the specified port number.
         """
-        if IP_PARTNER_PORT_NUMBER in self.ports[port_number]:
-            return self.ports[port_number][IP_PARTNER_PORT_NUMBER]
-        else:
-            raise KeyError('IP partner not found for port ' + port_number)
+        return self.ports[port_number][IP_PARTNER_PORT_NUMBER]
 
     def get_rx_bytes(self, port_number):
         """
@@ -169,7 +136,23 @@ class SwitchStats:
         """
         return self.ports[port_number][TX_PACKETS]
 
-    def _update_stat(self, port_number, buffer_index_key, buffer_key, stat_key, stat_value, lldp_noise=0):
+    def get_sdn_rx_bytes(self, port_number):
+        return self._get_sdn_stat(port_number, SDN_RX_BYTES)
+
+    def get_sdn_tx_bytes(self, port_number):
+        return self._get_sdn_stat(port_number, SDN_TX_BYTES)
+
+    def get_sdn_rx_packets(self, port_number):
+        return self._get_sdn_stat(port_number, SDN_RX_PACKETS)
+
+    def get_sdn_tx_packets(self, port_number):
+        return self._get_sdn_stat(port_number, SDN_TX_PACKETS)
+
+    def _get_sdn_stat(self, port_number, stat_key):
+        self._update_sdn_stats()
+        return self.ports[port_number][stat_key]
+
+    def _update_stat(self, port_number, stat_key, stat_value, lldp_noise=0):
         """
         Update stat value filling a circular buffer of config.DELTA_WINDOW for the specified port.
 
@@ -181,48 +164,12 @@ class SwitchStats:
         log.debug("Update %s stat for %s datapath, port %s with value: %s", stat_key, self.data_path_id, port_number,
                   stat_value)
         port = self.ports[port_number]
-        log.debug("Current values for port %s, stat %s. Value: %s, Buffer index: %s, Buffer: %s", port_number, stat_key,
-                  port[stat_key],
-                  port[buffer_index_key],
-                  port[buffer_key])
-        # Time interval definition
-        time_interval_end = port[buffer_index_key]
-        time_interval_start = (time_interval_end - 1) % config.DELTA_WINDOW
-        log.debug("Time interval start for port %s: %s", port_number, time_interval_start)
-        log.debug("Time interval end for port %s: %s", port_number, time_interval_end)
-        # stat count recorded @ time_interval_start
-        time_interval_start_count = port[buffer_key][time_interval_start]
-        log.debug("Count @ time_interval_start for %s (buffer_key: %s), port %s: %s", stat_key, buffer_key, port_number,
-                  time_interval_start_count)
-        # update stat if necessary (time_interval_start_count == 0 if the buffer is partially empty)
-        if time_interval_start_count != 0:
-            updated_value = stat_value - time_interval_start_count - lldp_noise
-            log.debug("Updating %s for port %s with value: %s", stat_key, port_number, updated_value)
-            port[stat_key] += updated_value
-            log.debug("New value for %s for port %s with value: %s", stat_key, port_number, port[stat_key])
-        else:
-            log.debug("Update for %s not necessary for port %s as count @ time_interval_start is 0", stat_key,
-                      port_number)
-        log.debug("Current values for port %s, stat %s. Current value: %s, Buffer index: %s, Buffer: %s", port_number,
-                  stat_key,
-                  port[stat_key],
-                  port[buffer_index_key],
-                  port[buffer_key])
-        # update time interval start
-        time_interval_start_updated_value = (port[buffer_index_key] + 1) % config.DELTA_WINDOW
-        log.debug("Update count @ time_interval_start for %s buffer, port %s with value: %s", buffer_index_key,
-                  port_number,
-                  time_interval_start_updated_value)
-        port[buffer_index_key] = time_interval_start_updated_value
-
-        # update stat count received @ time_interval_end
-        port[buffer_key][time_interval_end] = stat_value
-        log.debug("Update count @ time_interval_end for %s buffer, port %s with value: %s", buffer_key, port_number,
-                  stat_value)
-        log.debug("Current values for port %s, stat %s. Value: %s, Buffer index: %s, Buffer: %s", port_number, stat_key,
-                  port[stat_key],
-                  port[buffer_index_key],
-                  port[buffer_key])
+        log.debug("Current values for port %s, stat %s. Value: %s", port_number, stat_key, port[stat_key])
+        updated_value = stat_value - lldp_noise
+        log.debug("Updating %s for port %s with value: %s", stat_key, port_number, updated_value)
+        port[stat_key] += updated_value
+        log.debug("New value for %s for port %s with value: %s", stat_key, port_number, port[stat_key])
+        log.debug("Current values for port %s, stat %s. Current value: %s", port_number, stat_key, port[stat_key])
 
     def set_rx_bytes(self, port_number, rx_bytes, lldp_noise=0):
         """
@@ -233,7 +180,7 @@ class SwitchStats:
         :param lldp_noise: LLDP traffic to subtract to rx_bytes, defaults to 0
         :return:
         """
-        self._update_stat(port_number, _RX_BYTES_BUFFER_INDEX, _RX_BYTES_BUFFER, RX_BYTES, rx_bytes, lldp_noise)
+        self._update_stat(port_number, RX_BYTES, rx_bytes, lldp_noise)
 
     def set_tx_bytes(self, port_number, tx_bytes, lldp_noise=0):
         """
@@ -244,7 +191,7 @@ class SwitchStats:
         :param lldp_noise: LLDP traffic to subtract to rx_bytes, defaults to 0
         :return:
         """
-        self._update_stat(port_number, _TX_BYTES_BUFFER_INDEX, _TX_BYTES_BUFFER, TX_BYTES, tx_bytes, lldp_noise)
+        self._update_stat(port_number, TX_BYTES, tx_bytes, lldp_noise)
 
     def set_rx_packets(self, port_number, rx_packets, lldp_noise=0):
         """
@@ -255,8 +202,7 @@ class SwitchStats:
         :param lldp_noise: LLDP traffic to subtract to rx_bytes, defaults to 0
         :return:
         """
-        self._update_stat(port_number, _RX_PACKETS_BUFFER_INDEX, _RX_PACKETS_BUFFER, RX_PACKETS, rx_packets,
-                          lldp_noise)
+        self._update_stat(port_number, RX_PACKETS, rx_packets, lldp_noise)
 
     def set_tx_packets(self, port_number, tx_packets, lldp_noise=0):
         """
@@ -267,109 +213,52 @@ class SwitchStats:
         :param lldp_noise: LLDP traffic to subtract to rx_bytes, defaults to 0
         :return:
         """
-        self._update_stat(port_number, _TX_PACKETS_BUFFER_INDEX, _TX_PACKETS_BUFFER, TX_PACKETS, tx_packets,
-                          lldp_noise)
+        self._update_stat(port_number, TX_PACKETS, tx_packets, lldp_noise)
 
-    def _get_sdn_stat(self, port_number, stat_index_key, stat_key):
-        port = self.ports[port_number]
-        index = (port[stat_index_key] - 1) % config.DELTA_WINDOW
-        return port[stat_key][index]
-
-    def get_sdn_rx_bytes(self, port_number):
-        """
-        Return the received SDN traffic for the specified port.
-
-        :param port_number:
-        :return: SDN traffic expressed in bytes
-        """
-        return self._get_sdn_stat(port_number, _SDN_RX_BYTES_BUFFER_INDEX, _SDN_RX_BYTES_BUFFER)
-
-    def get_sdn_rx_packets(self, port_number):
-        """
-        Return the received SDN traffic for the specified port.
-
-        :param port_number:
-        :return: SDN traffic expressed in packets
-        """
-        return self._get_sdn_stat(port_number, _SDN_RX_PACKETS_BUFFER_INDEX, _SDN_RX_PACKETS_BUFFER)
-
-    def get_sdn_tx_bytes(self, port_number):
-        """
-        Return the transmitted SDN traffic for the specified port.
-
-        :param port_number:
-        :return: SDN traffic expressed in bytes
-        """
-        return self._get_sdn_stat(port_number, _SDN_TX_BYTES_BUFFER_INDEX, _SDN_TX_BYTES_BUFFER)
-
-    def get_sdn_tx_packets(self, port_number):
-        """
-        Return the transmitted SDN traffic for the specified port.
-
-        :param port_number:
-        :return: SDN traffic expressed in packets
-        """
-        return self._get_sdn_stat(port_number, _SDN_TX_PACKETS_BUFFER_INDEX, _SDN_TX_PACKETS_BUFFER)
-
-    def __get_sdn_rx_bytes(self, port_number):
+    def __compute_sdn_rx_bytes(self, port_number):
         """
         Compute and return received SDN traffic for the specified port.
 
         :param port_number:
         :return: Received SDN traffic in bytes
         """
-        try:
-            ip_partner_port_number = self.get_ip_partner_port_number(port_number)
-        except KeyError:
-            return -1
         # received total - ip sent to partner
-        return self.get_rx_bytes(port_number) - self.get_tx_bytes(ip_partner_port_number)
+        return self.__compute_sdn_stat(port_number, RX_BYTES, TX_BYTES)
 
-    def __get_sdn_rx_packets(self, port_number):
+    def __compute_sdn_rx_packets(self, port_number):
         """
         Compute and return received SDN traffic for the specified port.
 
         :param port_number:
         :return: Received SDN traffic in packets
         """
-        try:
-            ip_partner_port_number = self.get_ip_partner_port_number(port_number)
-        except KeyError:
-            return -1
-        # received total - ip sent to partner
-        return self.get_rx_packets(port_number) - self.get_tx_packets(ip_partner_port_number)
+        return self.__compute_sdn_stat(port_number, RX_PACKETS, TX_PACKETS)
 
-    def __get_sdn_tx_bytes(self, port_number):
+    def __compute_sdn_tx_bytes(self, port_number):
         """
         Compute and return transmitted SDN traffic for the specified port.
 
         :param port_number:
         :return: Transmitted SDN traffic in bytes
         """
-        try:
-            ip_partner_port_number = self.get_ip_partner_port_number(port_number)
-        except KeyError:
-            return -1
-        sdn_bytes = 0
-        # sent total - ip received by partner
-        sdn_bytes += self.get_tx_bytes(port_number) - self.get_rx_bytes(ip_partner_port_number)
-        return sdn_bytes
+        return self.__compute_sdn_stat(port_number, TX_BYTES, RX_BYTES)
 
-    def __get_sdn_tx_packets(self, port_number):
+    def __compute_sdn_tx_packets(self, port_number):
         """
         Compute and return transmitted SDN traffic for the specified port.
 
         :param port_number:
         :return: Transmitted SDN traffic in packets
         """
+        # sent total - ip received by partner
+        return self.__compute_sdn_stat(port_number, TX_PACKETS, RX_PACKETS)
+
+    def __compute_sdn_stat(self, port_number, grand_total_stat, stat_to_subtract):
         try:
             ip_partner_port_number = self.get_ip_partner_port_number(port_number)
+            return self.ports[port_number][grand_total_stat] - self.ports[ip_partner_port_number][stat_to_subtract]
         except KeyError:
-            return -1
-        sdn_packets = 0
-        # sent total - ip received by partner
-        sdn_packets += self.get_tx_packets(port_number) - self.get_rx_packets(ip_partner_port_number)
-        return sdn_packets
+            return 0
 
     @staticmethod
     def _has_rx_lldp_noise(port_name, port_number):
@@ -387,29 +276,27 @@ class SwitchStats:
         """
         self.__seconds_from_start += 1
         for port_number in self.ports:
-            p = self.ports[port_number]
+            port = self.ports[port_number]
             rx_noise = SwitchStats._has_rx_lldp_noise(self.get_port_name(port_number), port_number)
-            t1_sdn = p[_SDN_RX_BYTES_BUFFER_INDEX]
-            p[_SDN_RX_BYTES_BUFFER_INDEX] = (p[_SDN_RX_BYTES_BUFFER_INDEX] + 1) % config.DELTA_WINDOW
-            p[_SDN_RX_BYTES_BUFFER][t1_sdn] = self.__get_sdn_rx_bytes(port_number) + (
-                self.__seconds_from_start * config.LLDP_NOISE_BYTE_S * rx_noise)
-            t1_sdn = p[_SDN_TX_BYTES_BUFFER_INDEX]
-            p[_SDN_TX_BYTES_BUFFER_INDEX] = (p[_SDN_TX_BYTES_BUFFER_INDEX] + 1) % config.DELTA_WINDOW
-            p[_SDN_TX_BYTES_BUFFER][t1_sdn] = self.__get_sdn_tx_bytes(port_number) - (
-                self.__seconds_from_start * config.LLDP_NOISE_BYTE_S)
-            t1_sdn = p[_SDN_RX_PACKETS_BUFFER_INDEX]
-            p[_SDN_RX_PACKETS_BUFFER_INDEX] = (p[_SDN_RX_PACKETS_BUFFER_INDEX] + 1) % config.DELTA_WINDOW
-            p[_SDN_RX_PACKETS_BUFFER][t1_sdn] = self.__get_sdn_rx_packets(port_number) + (
-                self.__seconds_from_start * config.LLDP_NOISE_PACK_S * rx_noise)
-            t1_sdn = p[_SDN_TX_PACKETS_BUFFER_INDEX]
-            p[_SDN_TX_PACKETS_BUFFER_INDEX] = (p[_SDN_TX_PACKETS_BUFFER_INDEX] + 1) % config.DELTA_WINDOW
-            p[_SDN_TX_PACKETS_BUFFER][t1_sdn] = self.__get_sdn_tx_packets(port_number) - (
-                self.__seconds_from_start * config.LLDP_NOISE_PACK_S)
+            sdn_rx_bytes = self.__compute_sdn_rx_bytes(port_number)
+            port[SDN_RX_BYTES] = 0 if sdn_rx_bytes == 0 else sdn_rx_bytes + (self.__seconds_from_start *
+                                                                             config.LLDP_NOISE_BYTE_S * rx_noise)
+            sdn_tx_bytes = self.__compute_sdn_tx_bytes(port_number)
+            port[SDN_TX_BYTES] = 0 if sdn_tx_bytes == 0 else sdn_tx_bytes - (self.__seconds_from_start *
+                                                                             config.LLDP_NOISE_BYTE_S)
+            sdn_rx_packets = self.__compute_sdn_rx_packets(port_number)
+            port[SDN_RX_PACKETS] = 0 if sdn_rx_packets == 0 else sdn_rx_packets + (self.__seconds_from_start *
+                                                                                   config.LLDP_NOISE_PACK_S * rx_noise)
+            sdn_tx_packets = self.__compute_sdn_tx_packets(port_number)
+            port[SDN_TX_PACKETS] = 0 if sdn_tx_packets == 0 else sdn_tx_packets - (self.__seconds_from_start *
+                                                                                   config.LLDP_NOISE_PACK_S)
 
     def get_current_values(self, port_number):
         self._update_sdn_stats()
-        return {RX_BYTES: self.get_rx_bytes(port_number), TX_BYTES: self.get_tx_bytes(port_number),
-                RX_PACKETS: self.get_rx_packets(port_number), TX_PACKETS: self.get_tx_packets(port_number),
+        return {RX_BYTES: self.get_rx_bytes(port_number),
+                TX_BYTES: self.get_tx_bytes(port_number),
+                RX_PACKETS: self.get_rx_packets(port_number),
+                TX_PACKETS: self.get_tx_packets(port_number),
                 SDN_RX_BYTES: self.get_sdn_rx_bytes(port_number),
                 SDN_TX_BYTES: self.get_sdn_tx_bytes(port_number),
                 SDN_RX_PACKETS: self.get_sdn_rx_packets(port_number),
--- tests/switch_stats_test.py
+++ tests/switch_stats_test.py
@@ -14,17 +14,20 @@ class TestSwitchStats(object):
     def test_add_port(self):
         print("Testing SwitchStats add port")
         switch_stat = SwitchStats('datapath-id')
-        port_number = '10'
+        port_number = 10
+        port_name = 'cro'
         switch_stat.add_port(port_number)
+        switch_stat.set_port_name(port_number, port_name)
         assert port_number in switch_stat.ports
-        assert switch_stat.get_rx_bytes(port_number) is 0
-        assert switch_stat.get_rx_packets(port_number) is 0
-        assert switch_stat.get_tx_bytes(port_number) is 0
-        assert switch_stat.get_tx_packets(port_number) is 0
-        assert switch_stat.get_sdn_rx_bytes(port_number) is 0
-        assert switch_stat.get_sdn_rx_packets(port_number) is 0
-        assert switch_stat.get_sdn_tx_bytes(port_number) is 0
-        assert switch_stat.get_sdn_tx_packets(port_number) is 0
+        assert port_name == switch_stat.get_port_name(port_number)
+        assert switch_stat.get_rx_bytes(port_number) == 0
+        assert switch_stat.get_rx_packets(port_number) == 0
+        assert switch_stat.get_tx_bytes(port_number) == 0
+        assert switch_stat.get_tx_packets(port_number) == 0
+        assert switch_stat.get_sdn_rx_bytes(port_number) == 0
+        assert switch_stat.get_sdn_rx_packets(port_number) == 0
+        assert switch_stat.get_sdn_tx_bytes(port_number) == 0
+        assert switch_stat.get_sdn_tx_packets(port_number) == 0
 
     def test_update_rx_bytes(self):
         print("Testing SwitchStats add port")
@@ -32,7 +35,8 @@ class TestSwitchStats(object):
         port_number = '10'
         switch_stat.add_port(port_number)
         rx_bytes_value_init = 100
+        rx_bytes_final_value = 0
         for rx_bytes_value in range(rx_bytes_value_init, rx_bytes_value_init + config.DELTA_WINDOW):
             switch_stat.set_rx_bytes(port_number, rx_bytes_value)
-        print("Current value for rx_bytes: " + str(switch_stat.get_rx_bytes(port_number)))
-        assert switch_stat.get_rx_bytes(port_number) is rx_bytes_value
+            rx_bytes_final_value += rx_bytes_value
+        assert switch_stat.get_rx_bytes(port_number) == rx_bytes_final_value
