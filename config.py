12c39106477fb6cce26948be4b46173ad30886a6
Marco Ferrari <ferrari.marco@gmail.com>
2015-11-11 18:32:07 +0100

    Removed unnecessary comments from config.py

--- config.py
+++ config.py
@@ -15,27 +15,3 @@ REQUEST_INTERVAL = 1  # Stats request time interval. MUST BE 1
 DELTA_WINDOW = 20  # RATE WINDOWS
 LLDP_NOISE_BYTE_S = 19
 LLDP_NOISE_PACK_S = 0.365
-
-'''
-
-                                                OUT=out+lldp        OUT=out+lldp
-                                                IN =in              IN =in
-
-                                          +-------|vi0 |------|vi1 |------|vi2 |----------------+
-                                          |                                                     |
-                                          |                                                     |
-    OUT=out+lldp                        ----                                                    |
-    IN =in                              eth0                 SWITCH                             |
-                                        ----                                                    |
-    sdn_out=out-in(vi)=OUT-lldp-in(vi)    |                                                     |
-    sdn_in =in-out(vi)=IN-OUT(vi)-lldp    |                                                     |
-                                          |                                                     |
-                                          |                                                     |
-                                          +-------------------|eth1|------|eth2|----------------+
-
-                                                            OUT=out+lldp
-                                                            IN =in +lldp
-                                                            sdn_out=out-in(vi)=OUT-lldp-IN(vi)
-                                                            sdn_in =in-out(vi)=IN-lldp-OUT(vi)+lldp=IN-OUT(vi)
-
-'''
