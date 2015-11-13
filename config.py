3397047b3db2d6ca57e62fca2a392434a0d4f386
Marco Ferrari <ferrari.marco@gmail.com>
2015-11-13 18:40:28 +0100

    Removed unused config variables

--- config.py
+++ config.py
@@ -9,13 +9,11 @@ LOG_LEVEL = logging.ERROR
 
 # RRD config
 RRD_STEP = "300"
-RRD_STORE_PATH = BASE_PATH+"rrd/"  # /home/user/workspace/OSHI-Monitoring/rrd/
+RRD_STORE_PATH = BASE_PATH+"rrd/"
 RRD_DATA_SOURCE_TYPE = "GAUGE"
 RRD_DATA_SOURCE_HEARTBEAT = "600"
 
 # Traffic monitor config
-PORT_BYTES_STATS_PATH = BASE_PATH+"port_stats/bytes_stats"
-PORT_PACKETS_STATS_PATH = BASE_PATH+"port_stats/packets_stats"
 REQUEST_INTERVAL = 1  # Stats request time interval. MUST BE 1
 DELTA_WINDOW = 20  # RATE WINDOWS
 LLDP_NOISE_BYTE_S = 19
