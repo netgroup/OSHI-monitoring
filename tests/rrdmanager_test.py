290937f5d356bab0adc73506bcf05447596ee650
Marco Ferrari <ferrari.marco@gmail.com>
2015-11-13 18:56:39 +0100

    Refactored tests to match new class structure

--- tests/rrdmanager_test.py
+++ tests/rrdmanager_test.py
@@ -8,11 +8,11 @@ class TestRRDManager(object):
 
     def test_rrdmanager_init(self):
         print("RRDManager Initialization Test: RDD file name")
-        device_name = "device_name"
-        port_number = 100
+        port_name = "port_name"
+        rrd_file_name = port_name + '.rrd'
         rrd_data_source = RRDDataSource("port_stat_name", config.RRD_DATA_SOURCE_TYPE,
                                         config.RRD_DATA_SOURCE_HEARTBEAT)
         rrd_data_sources = [rrd_data_source]
-        rrd_manager = RRDManager(device_name, port_number, rrd_data_sources)
-        assert rrd_manager.filename == join(config.RRD_STORE_PATH, str(device_name) + "_" + str(port_number) + ".rrd")
+        rrd_manager = RRDManager(rrd_file_name, rrd_data_sources)
+        assert rrd_manager.filename == join(config.RRD_STORE_PATH, rrd_file_name)
         print("Built RRD file name: {}".format(rrd_manager.filename))
