ce5a75bea61973536dd2e6dd73e6199923ea79b4
Marco Ferrari <ferrari.marco@gmail.com>
2015-11-25 19:03:03 +0100

    Removed outdated comments from log configuration

--- __init__.py
+++ __init__.py
@@ -4,15 +4,10 @@ import config
 
 log = logging.getLogger('oshi_monitoring')
 log.setLevel(config.LOG_LEVEL)
-
-# create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(config.LOG_LEVEL)
-# create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 ch.setFormatter(formatter)
-
-# add the handlers to logger
 log.addHandler(ch)
 
 if config.ENABLE_FILE_LOGGING:
