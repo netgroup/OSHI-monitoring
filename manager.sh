9d6eb46793b1b7a25376c6845667fef60abefa54
Marco Ferrari <ferrari.marco@gmail.com>
2015-11-25 19:45:38 +0100

    Removed unnecessary python source file from command line parameters when launching RYU

--- manager.sh
+++ manager.sh
@@ -56,7 +56,7 @@ elif [[ ${MODE} = 'runryu' ]]
 	then
 	  echo "Running RYU..."
 	  cd /home/user/workspace/dreamer-ryu/ryu/app/
-	  ryu-manager --observe-links traffic_monitor.py rrdmanager.py
+	  ryu-manager --observe-links traffic_monitor.py
 else 
           echo "Unrecognized option. Available options are: setup, runmininet and runryu. This script will now terminate."
           exit 1
